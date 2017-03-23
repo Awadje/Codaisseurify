14fac42696a4f809d20903024f126b53abd3c8ab
Awadje <michaelawad85@gmail.com>
2017-03-23 20:36:23 +0100

    minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'artists#index'
 
-  resources :songs, only: [:show]
+  resources :songs, only: [:show, :delete]
   resources :artists, only: [:edit, :update]
 
   resources :artists do
-              resources :songs, only: [:show, :new, :create, :update]
+              resources :songs, only: [:show, :new, :create, :update, :edit]
             end
 
   resources :photo, only: [:show]
